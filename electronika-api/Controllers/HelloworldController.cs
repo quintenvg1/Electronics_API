592d4bfcd1ac1a06d4b90ffc293a4c65a86ba1c0
quintenvg1 <38690465+quintenvg1@users.noreply.github.com>
2020-03-26 13:15:22 +0100

    added first controller for test purposes

---
+++ electronika-api/Controllers/HelloworldController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace electronika_api.Controllers
+{
+    [Route("hi")]
+    [ApiController]
+    public class HelloworldController : ControllerBase
+    {
+        public IActionResult hello()
+        {
+            return Content("hello");
+        }
+    }
+}
