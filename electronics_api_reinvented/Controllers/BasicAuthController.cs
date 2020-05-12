9163117ffa5750538c6939adb896acf1855ba480
quintenvg1 <38690465+quintenvg1@users.noreply.github.com>
2020-05-12 11:51:38 +0200

    added basic auth controller
    starting the basics of authorisation

---
+++ electronics_api_reinvented/Controllers/BasicAuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace electronics_api_reinvented.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BasicAuthController : Controller
+	{
+
+		public IActionResult Login()
+		{
+			Response.Headers.Add("WWW-Authenticate", "basic");
+			return Unauthorized();
+		}
+	}
+}
