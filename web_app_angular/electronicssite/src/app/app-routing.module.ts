70635d3961403fce92ece1a5af15b04ef1e55fcb
Quinten Van Ginderen <quintevg1@gmail.com>
2020-05-15 12:46:03 +0200

    added routing and apiservice to the app

--- web_app_angular/electronicssite/src/app/app-routing.module.ts
+++ web_app_angular/electronicssite/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DeviceAddComponent } from './device-add/device-add.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'create', component: DeviceAddComponent }, //route to the device add form
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ web_app_angular/electronicssite/src/app/device-add/device-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-add',
+  templateUrl: './device-add.component.html',
+  styleUrls: ['./device-add.component.css']
+})
+export class DeviceAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
